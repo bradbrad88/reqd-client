139884fa3208a6d3c87a5ad7e40fc78c281e6aa7
Brad Teague <b_rad88@live.com>
2023-09-07 06:49:31 +0930

    Clean up useAreas hooks and consumers

--- src/features/areas/AddProductToArea.tsx
+++ src/features/areas/AddProductToArea.tsx
@@ -1,8 +1,5 @@
-import { addProductToVenueArea, removeProductFromVenueArea } from "api/areas";
-import { keys } from "api/querieFactory";
-import { useMutation, useQueryClient } from "react-query";
 import { useOutletContext, useParams } from "react-router-dom";
-import { useAddProductToArea } from "src/hooks/useAreas";
+import { useAddProductToArea, useRemoveProductFromArea } from "src/hooks/useAreas";
 import { useProductsContext, useVenueContext } from "src/hooks/useContexts";
 import { ProductDetail } from "src/hooks/useProducts";
 
@@ -43,51 +40,10 @@ type ProductProps = {
 function Product({ id, displayName, active }: ProductProps) {
   const { venueId } = useVenueContext();
   const { areaId } = useParams<{ areaId: string }>();
-  const client = useQueryClient();
-  const { mutate: removeProduct } = useMutation(removeProductFromVenueArea, {
-    onMutate: async () => {
-      const key = keys.detail(venueId, "areas", areaId!);
-      await client.cancelQueries(key);
-      const previous = client.getQueryData(key) as {
-        products: { id: string; displayName: string }[];
-      };
-      if (previous) {
-        client.setQueryData(key, {
-          ...previous,
-          products: previous.products.filter(product => product.id !== id),
-        });
-      }
-      return { previous };
-    },
-    onSettled: (_, __, vars) => {
-      const key = keys.detail(vars.venueId, "areas", vars.areaId);
-      client.invalidateQueries(key);
-    },
-    onError: (_, vars, context) => {
-      const key = keys.detail(vars.venueId, "areas", vars.areaId);
-      if (context?.previous) {
-        client.setQueryData(key, context.previous);
-      }
-    },
-  });
+  const { mutate: removeProduct } = useRemoveProductFromArea(venueId, areaId!);
+
   const { mutate: addProduct } = useAddProductToArea(venueId, areaId!);
-  // const { mutate: addProduct } = useMutation(addProductToVenueArea, {
-  //   onMutate: async () => {
-  //     const key = keys.detail(venueId, "areas", areaId!);
-  //     await client.cancelQueries(key);
-  //     const prev = client.getQueryData(key) as {
-  //       products: { id: string; displayName: string }[];
-  //     };
-  //     client.setQueryData(key, {
-  //       ...prev,
-  //       products: prev.products.concat({ id, displayName }),
-  //     });
-  //   },
-  //   onSettled: (_, __, { venueId }) => {
-  //     const key = keys.detail(venueId, "areas", areaId!);
-  //     client.invalidateQueries(key);
-  //   },
-  // });
+
   const onClick = () => {
     if (active) {
       removeProduct({ venueId, areaId: areaId!, productId: id });
@@ -95,6 +51,7 @@ function Product({ id, displayName, active }: ProductProps) {
       addProduct({ venueId, areaId: areaId!, productId: id });
     }
   };
+
   return (
     <div onClick={onClick}>
       <div className="flex justify-between text-xl">
