97cb5abf5a38699860bfa7ceacf4a447968a9272
Brad Teague <b_rad88@live.com>
2023-09-07 09:06:34 +0930

    Add functionality for reaching 'CreateArea' component

--- src/features/areas/AreaList.tsx
+++ src/features/areas/AreaList.tsx
@@ -1,16 +1,29 @@
 import { useAreaList } from "src/hooks/useAreas";
 import Area from "./Area";
 import { useVenueContext } from "src/hooks/useContexts";
+import CallToAction from "common/CallToAction";
+import { useNavigate } from "react-router-dom";
+import FlexList from "common/FlexList";
 
 const AreaList = () => {
   const { venueId } = useVenueContext();
   const { data: areaList } = useAreaList(venueId);
+  const nav = useNavigate();
 
   const renderAreas = () => {
     return areaList.map(area => <Area key={area.id} {...area} />);
   };
 
-  return <div>{renderAreas()}</div>;
+  const onCreate = () => {
+    nav("create");
+  };
+
+  return (
+    <div className="flex flex-col gap-5">
+      <CallToAction action={onCreate}>Create</CallToAction>
+      <FlexList>{renderAreas()}</FlexList>
+    </div>
+  );
 };
 
 export default AreaList;
