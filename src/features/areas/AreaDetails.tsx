7db9e2101e2e771b85508e2f313eb90bf144cc42
Brad Teague <b_rad88@live.com>
2023-09-07 06:51:05 +0930

    Clean up AreaDetails component

--- src/features/areas/AreaDetails.tsx
+++ src/features/areas/AreaDetails.tsx
@@ -1,20 +1,19 @@
 import { Outlet, useParams } from "react-router-dom";
 import { useAreaDetail } from "src/hooks/useAreas";
-import AreaProducts from "./AreaProducts";
 import { useVenueContext } from "src/hooks/useContexts";
 
 const AreaDetails = () => {
   const { venueId } = useVenueContext();
   const { areaId } = useParams<{ areaId: string }>();
-  const { data } = useAreaDetail(areaId!, venueId);
+  const { data, status } = useAreaDetail(areaId!, venueId);
 
+  if (status === "error") return <div>Product not found</div>;
   if (!data) return <div>Loading</div>;
 
   return (
     <div>
       <h2>{data.areaName}</h2>
       <Outlet context={{ areaProducts: data.products }} />
-      {/* <AreaProducts products={data.products} /> */}
     </div>
   );
 };
