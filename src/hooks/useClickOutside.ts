70d421cb4809995daa7c54adab0dfd322260dbac
Brad Teague <b_rad88@live.com>
2024-01-13 08:46:53 +1030

    Fix: useClickOutside
    Save ref.current to a variable so it can be correctly referenced in onClick callback

--- src/hooks/useClickOutside.ts
+++ src/hooks/useClickOutside.ts
@@ -2,9 +2,10 @@ import { RefObject, useEffect } from "react";
 
 const useClickOutside = (ref: RefObject<HTMLDivElement>, cb: () => void) => {
   useEffect(() => {
+    const element = ref.current;
     const onClick = (event: MouseEvent) => {
-      if (!ref.current) return;
-      if (ref.current.contains(event.target as HTMLDivElement)) return;
+      if (!element) return;
+      if (element.contains(event.target as HTMLDivElement)) return;
       cb();
     };
     window.addEventListener("click", onClick);
