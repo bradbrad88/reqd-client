aa1ea35b750ea14ebf9f0d60a87da31b29107807
Brad Teague <b_rad88@live.com>
2024-01-14 20:37:27 +1030

    Fix: Searchbar component given autoFocus prop and using id correclty with label

--- src/common/SearchBar.tsx
+++ src/common/SearchBar.tsx
@@ -1,4 +1,4 @@
-import { ChangeEventHandler } from "react";
+import { ChangeEventHandler, useId } from "react";
 import useDebounce from "src/hooks/useDebounce";
 import { Input } from "./Inputs";
 import SearchIcon from "./icons/Search";
@@ -6,18 +6,22 @@ import SearchIcon from "./icons/Search";
 type Props = {
   onSearch: (query: string) => void;
   timer?: number;
+  autoFocus?: boolean;
+  id?: string;
 };
 
-const SearchBar = ({ onSearch, timer = 300 }: Props) => {
+const SearchBar = ({ onSearch, timer = 300, autoFocus = false, id }: Props) => {
   const onChange: ChangeEventHandler<HTMLInputElement> = e => {
     onSearch(e.target.value);
   };
 
+  const generatedId = useId();
+
   const { debounce } = useDebounce(onChange, timer);
 
   return (
-    <div className="relative">
-      <Input onChange={debounce} />
+    <div className="relative w-full">
+      <Input id={id || generatedId} onChange={debounce} autoFocus={autoFocus} tabIndex={0} />
       <span className="absolute right-3 top-1/2 -translate-y-1/2">
         <SearchIcon />
       </span>
