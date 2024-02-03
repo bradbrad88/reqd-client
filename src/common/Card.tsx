bb2b8436f4d3777cd695ae0479025487e553ae7a
Brad Teague <b_rad88@live.com>
2024-02-03 15:36:44 +1030

    Chore: Give ability to provide custom classes to Card component

--- src/common/Card.tsx
+++ src/common/Card.tsx
@@ -1,6 +1,13 @@
-const Card = ({ children }: { children: React.ReactNode }) => {
+import { cn } from "utils/cn";
+
+const Card = ({ className, children }: { className?: string; children: React.ReactNode }) => {
   return (
-    <div className="rounded-lg border-zinc-600 border-[1px] p-6 shadow-black shadow-md bg-zinc-900">
+    <div
+      className={cn(
+        "rounded-lg border-zinc-600 border-[1px] p-6 shadow-black shadow-md bg-zinc-900",
+        className
+      )}
+    >
       {children}
     </div>
   );
